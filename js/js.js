07d9a3478b323d76dc227f355e0d7f4180fd4411
wawatai <wataiPublic@gmail.com>
2022-12-01 18:26:01 +0800

    111-12-01

---
+++ js/js.js
@@ -0,0 +1,13 @@
+$(function(){
+    //Swiper
+    var swiper = new Swiper('.swiper', {
+        autoHeight: true,
+        loop: true,
+        autoplay: {
+            delay: 1500,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: true,
+        },
+        slidesPerView: 1,
+    })
+})
